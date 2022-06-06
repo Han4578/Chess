4ec9e17f6aadb0715a583eb2e1b01bc51e5d6e6e
Han4578 <99018603+Han4578@users.noreply.github.com>
2022-06-06 13:03:07 +0800

    Added features
    Pieces are now visible on the board and can be moved around.
    Board can be reversed

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+import pieces from "./pieces.json" assert {type: 'json'};
+
+
 let board = document.getElementById('board');
+let reverseBtn = document.getElementById('reverse')
 
 const setupBoard = {
     setup() {
@@ -9,20 +13,20 @@ const setupBoard = {
         startGame()
     },
     whiteFirst(n) {
-        for (i = 1; i <= 8; i++) {
+        for (let i = 1; i <= 8; i++) {
             let tile = document.createElement('div');
             tile.classList.add('tile')
-            tile.dataset.number = n.toString() + i;
+            tile.dataset.number = String.fromCharCode(i + 96) + Math.abs(n - 9);
             (i % 2 == 0) ? tile.style.backgroundColor = 'black': '';
             board.appendChild(tile);
         }
     },
 
     blackFirst(n) {
-        for (i = 1; i <= 8; i++) {
+        for (let i = 1; i <= 8; i++) {
             let tile = document.createElement('div');
             tile.classList.add('tile')
-            tile.dataset.number = n.toString() + i;
+            tile.dataset.number = String.fromCharCode(i + 96) + Math.abs(n - 9);
             (i % 2 == 1) ? tile.style.backgroundColor = 'black': '';
             board.appendChild(tile);
         }
@@ -31,32 +35,49 @@ const setupBoard = {
 }
 
 function startGame() {
-    black.forEach(p => {
-        let piece = document.createElement('img')
-        piece.src = p.src
-        piece.setAttribute("draggable",true)
-        piece.classList.add('piece')
-        let locations = document.getElementsByClassName('tile')
+    pieces.forEach(p => {
+        p.location.forEach(l => {
+            var piece = document.createElement('img')
+            piece.src = p.src
+            piece.id = l
+            piece.setAttribute("draggable", true)
+            piece.classList.add('piece')
 
-        
-        Array.from(locations).forEach(l =>{
-            console.log(p.position.indexOf(13))
-            if(p.position.indexOf(13)){
-                l.appendChild(piece)
-            }
+            Array.from(board.children).forEach(tile => {
+                (tile.dataset.number == l) ? tile.appendChild(piece): ''
+            })
         })
     })
 }
-
-const black = [
-    bishop = {
-        src: 'images/b_bishop.png',
-        position: [13,16]
-    },
-    king = {
-        src: '',
-        position: [14]
-    },
-]
-
 setupBoard.setup()
+
+let piece = Array.from(document.getElementsByClassName('piece'))
+let tile = Array.from(document.getElementsByClassName('tile'))
+
+piece.forEach(p => {
+    p.addEventListener("dragstart", (e) => {
+        e.dataTransfer.setData("text/plain", e.target.id)
+    })
+
+})
+
+tile.forEach(t => {
+
+    t.addEventListener("dragover", (e) => {
+        e.preventDefault()
+    })
+
+    t.addEventListener("drop", (e) => {
+        e.preventDefault()
+
+        let droppedPiece = e.dataTransfer.getData('text/plain')
+        let droppedPieceId = document.getElementById(droppedPiece)
+        t.appendChild(droppedPieceId);
+
+        (t.children.length > 1)? t.removeChild(t.firstChild):""
+    })
+})
+
+reverseBtn.addEventListener('click', () => {
+    board.classList.toggle('reverse')
+})
